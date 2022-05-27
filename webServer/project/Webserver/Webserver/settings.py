d997c8c76375a376f56075b86c416cc96cc721df
warleon <51892390+warleon@users.noreply.github.com>
2022-05-27 02:26:52 -0500

    done migrations
    Co-Authored-By: BT <felix.blanco@utec.edu.pe>

--- webServer/project/Webserver/Webserver/settings.py
+++ webServer/project/Webserver/Webserver/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'BigTeta.apps.BigtetaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
