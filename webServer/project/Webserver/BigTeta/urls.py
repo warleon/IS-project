c0b783e3bbe92f77f091c3502a42113ea01c2960
eduardosalas1 <eduardo.salas@utec.edu.pe>
2022-05-27 14:22:39 -0500

    nueva navbar

--- webServer/project/Webserver/BigTeta/urls.py
+++ webServer/project/Webserver/BigTeta/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.base import TemplateView
 from . import views
 app_name = 'BigTeta'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('home', views.index, name='index'),
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
 
--- webServer/project/Webserver/Webserver/settings.py
+++ webServer/project/Webserver/Webserver/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -118,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
