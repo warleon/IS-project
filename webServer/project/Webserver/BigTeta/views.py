085c5f0f2a8c83d863c469e60c1af4ae7f6632c6
BTbackM <felix.blanco@utec.edu.pe>
2022-06-03 15:19:18 -0500

    feat: User authentication methods

--- webServer/project/Webserver/BigTeta/views.py
+++ webServer/project/Webserver/BigTeta/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages 
-from django.contrib.auth.models import auth
-from .models import User
+from django.contrib.auth.models import User, auth
 
 from django.contrib.auth import logout
 
@@ -13,10 +12,12 @@ def login(request):
         username = request.POST['username']
         password = request.POST['password']
 
-        user = auth.authenticate(username=username, password=password)
+        user = auth.authenticate(request, username=username, password=password)
+        # user = User.objects.filter(username=username)
         if user is not None:
             auth.login(request, user)
-            return redirect('home')
+            messages.info(request, 'Username Auth')
+            return redirect('BigTeta:home')
         else:
             messages.info(request, "Invalid Username or Password")
             return redirect('BigTeta:login')
@@ -32,18 +33,18 @@ def register(request):
         if password1 == password2:
             if User.objects.filter(username=username).exists():
                 messages.info(request, 'Username Taken')
-                return redirect('register')
+                return redirect('BigTeta:register')
             elif User.objects.filter(email=email).exists():
                 messages.info(request, 'Email Taken')
-                return redirect('register')
+                return redirect('BigTeta:register')
             else:
-                user = User.objects.create(username=username, password=password1, email=email)
+                user = User.objects.create_user(username=username, password=password1, email=email)
                 user.save()
                 print("User created")
                 return redirect('BigTeta:login')
         else:
             messages.info(request, 'password not matched!')
-            return redirect('register')
+            return redirect('BigTeta:register')
     else:
         return render(request, 'register.html')
 
