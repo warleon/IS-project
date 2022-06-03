321a5f2b1a57bf7a78a3c0c70b1eb274599cdcd7
warleon <51892390+warleon@users.noreply.github.com>
2022-06-02 23:22:46 -0500

    db working

--- webServer/project/Webserver/Webserver/settings.py
+++ webServer/project/Webserver/Webserver/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'database',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
