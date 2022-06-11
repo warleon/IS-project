92831e89a3f86fcf386b075430608eac5a93c53d
warleon <51892390+warleon@users.noreply.github.com>
2022-06-10 20:46:48 -0500

    only logged users can upload

--- webServer/project/Webserver/BigTeta/urls.py
+++ webServer/project/Webserver/BigTeta/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.base import TemplateView
 from . import views
 app_name = 'BigTeta'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.showFiles, name='showVideos'),
     path('home', views.index, name='home'),
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
